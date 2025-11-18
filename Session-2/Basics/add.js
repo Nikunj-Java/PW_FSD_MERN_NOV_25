05524a4870d1b89df734790ddee58f49a1fb57e0
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-11-18 19:31:58 +0530

    JavaScript Basic code

---
+++ Session-2/Basics/add.js
@@ -0,0 +1,11 @@
+let a=10;
+
+let b=20;
+
+let sum= a + b;
+
+//console.log("sum of 10 +20 =",sum);
+console.log("sum of 10 +20 =",a + b);
+//console.log("Welcome to the world of javaScript")
+
+// you need node to be installed on your machine
