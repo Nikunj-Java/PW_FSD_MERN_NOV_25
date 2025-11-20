82ef18936a7ba3facf87583ef76099f61c4a95be
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-11-20 19:56:11 +0530

    Psuedo Codes in Algorithms

---
+++ Session-3/4. alogorithm.js
@@ -0,0 +1,18 @@
+//Problem Statement: write an alogorithm which can detect the weather as cloudy or sunny!
+
+// Psuedo Code
+/*
+    take input from user
+    if the temperature is greater than 30 degree 
+        then it is cloudy 
+    else it is sunny!
+*/
+
+//actual code based on Psuedo code
+let temperature=28;
+
+if(temperature<=30){
+    console.log("Weather is Cloudy")
+}else{
+    console.log("Weather is Sunny!")
+}
