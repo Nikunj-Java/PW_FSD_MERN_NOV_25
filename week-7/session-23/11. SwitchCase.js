65e294b5e190323f1684152cc8cf2978f51549e8
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-01-06 19:18:55 +0530

    Switch Case

---
+++ week-7/session-23/11. SwitchCase.js
@@ -0,0 +1,31 @@
+let text="JavaScript";
+
+switch (text[0]){
+    case 'a':
+        console.log("vowel");
+        break;
+         
+
+    case 'e':
+        console.log("vowel");
+        break;
+         
+
+    case 'i':
+        console.log("vowel");
+        break;
+         
+
+    case 'o':
+        console.log("vowel");
+        break;
+
+    case 'u':
+        console.log("vowel");
+        break;
+         
+
+   
+    default:
+        console.log("consonant");
+}
