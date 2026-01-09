a1ec1415e3c8f01cf7661cdeb3b30748394775a9
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-01-09 18:56:57 +0530

    JavaScript Functions

---
+++ week-7/session-24/09. RestParameters.js
@@ -0,0 +1,15 @@
+//syntax: function name(...num){ }
+
+function sum(...num){
+    var total=0;
+    for(let i of num){
+        total=total+i
+    }
+    return total;
+}
+
+console.log("Sum of 1,2,3: ",sum(1,2,3));// 1+2+3=6
+
+console.log("Sum of 1,2,3,4,5: ",sum(1,2,3,4,5)); //1+2+3+4+5=15
+
+console.log("Sum of 1,2,3,........,9,10: ",sum(1,2,3,4,5,6,7,8,9,10)); //1+2+3+4+5+6+7+8+9+10=55
---
+++ week-7/session-24/10. RestParameters.js
@@ -0,0 +1,9 @@
+function ShowItem(item1,item2,item3,...itemN){
+    console.log(item1);
+    console.log(item2);
+    console.log(item3);
+    console.log(itemN); //this is an array of rest of the items
+}
+
+
+ShowItem('apple','banana','mango','pineapple','strawberry','custered Apple');
