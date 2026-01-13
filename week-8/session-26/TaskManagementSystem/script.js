57d7dd5991cc0151f6e99287fadc54d20c593289
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-01-13 17:51:55 +0530

    Task management System

--- week-8/session-26/TaskManagementSystem/script.js
+++ week-8/session-26/TaskManagementSystem/script.js
@@ -1,4 +1,14 @@
+function Task(taskName){
+    this.taskName=taskName;
+    this.completed=false;
+}
 
+Task.prototype.toggleComplete=function(){
+    this.completed=!this.completed;
+}
+
+//task list array
+let tasks= []
 
 function addTask(){
     let input=document.getElementById("taskInput").value;
@@ -7,5 +17,39 @@ function addTask(){
         alert("please Enter a Task");
         return;
     }
-    
+
+    let task= new Task(input);
+    tasks.push(task);
+    console.log(tasks);
+    document.getElementById("taskInput").value="";
+
+    displaytasks();
+}
+
+//display task
+
+function displaytasks(){
+    let list= document.getElementById("taskList");
+    list.innerHTML="";
+
+    tasks.forEach((task,index)=>{
+        let li= document.createElement("li");
+        li.className=task.completed ? "Completed":"";
+        li.innerHTML=
+        ` ${task.taskName}
+        <button onClick="completedTask(${index})"> ✔ </button>
+        <button onClick="deleteTask(${index})"> X </button>
+        `
+        list.appendChild(li);
+    })
+}
+
+function completedTask(index){
+    tasks[index].toggleComplete();
+    displaytasks();
+}
+
+function deleteTask(index){
+    tasks.splice(index,1)
+    displaytasks();
 }
