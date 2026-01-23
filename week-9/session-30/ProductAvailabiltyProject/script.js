2f2426685541e7c602469b7b06b00b0411983fb5
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-01-23 18:51:38 +0530

    Product Availability Project

--- week-9/session-30/ProductAvailabiltyProject/script.js
+++ week-9/session-30/ProductAvailabiltyProject/script.js
@@ -1,27 +1,53 @@
 
 //product Constructor
-function Product(name,available){
-    this.name=name;
-    this.available=available;
+function Product(name, available) {
+    this.name = name;
+    this.available = available;
 }
 
 //product array
-let products= [];
+let products = [];
 
 
-function addProduct(){
-    let name=document.getElementById("productName").value;
+function addProduct() {
+    let name = document.getElementById("productName").value;
 
-    if(name === ""){
+    if (name === "") {
         alert("Please Enter Product Name!");
         return
     }
 
-    let newProduct= new Product(name,true);
+    let newProduct = new Product(name, true);
     products.push(newProduct);//push is array method
 
-    console.log(newProduct);
+    //console.log(newProduct);
+    displayProducts();
+    document.getElementById("productName").value="";
 
-
-    
 }
+
+//function to display Products
+function displayProducts() {
+    let list = document.getElementById("productList");
+    list.innerHTML = "";
+
+    products.forEach((product, index) => {
+        list.innerHTML += `
+          <li>
+            ${product.name}
+            <span class="${product.available ? 'available' : 'not-available'}">
+              ${product.available ? 'Available' : 'Out of Stock'}
+            </span>
+            <button class="${product.available ? 'out' : ''}" onclick="toggleStatus(${index})">
+              Toggle
+            </button>
+          </li>
+        `;
+    });
+}
+
+function toggleStatus(index) {
+    products[index].available = !products[index].available;
+    displayProducts();
+}
+
