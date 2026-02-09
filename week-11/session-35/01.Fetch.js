15ddcc3af0aaa1bc79aab12b7dc90bd5bcf72138
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-02-09 17:27:25 +0530

    REST

---
+++ week-11/session-35/01.Fetch.js
@@ -0,0 +1,16 @@
+
+function loadData(){
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response=> response.json())
+    .then(data=> console.log(data))
+    .catch(error=>console.error(error))
+}
+//loadData();
+
+function loadOneData(){
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(response=> response.json())
+    .then(data=> console.log(data))
+    .catch(error=>console.error(error))
+}
+//loadOneData();
