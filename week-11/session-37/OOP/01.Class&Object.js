688f6cae2ce2f223e1480a518dba63870a11c510
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-02-13 18:25:12 +0530

    Date Time

---
+++ week-11/session-37/OOP/01.Class&Object.js
@@ -0,0 +1,24 @@
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    greet(){
+        console.log(`Hello My Name Is ${this.name} and I am ${this.age} Years Old`);
+    }
+}
+
+let person1= new Person("Suhas",25);
+person1.greet();
+
+let person2= new Person("Danish",30);
+person2.greet();
+
+let person3= new Person("Nikhil",22);
+person3.greet();
+
+let person4= new Person("Ashi",12);
+person4.greet();
+
+let person5= new Person("Nikunj",36);
+person5.greet();
