d4aba8914ead173e26a63b5344f60f79081b23bf
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-02-13 17:12:49 +0530

    Date Time

---
+++ week-11/session-37/01. DateTime.js
@@ -0,0 +1,24 @@
+let today= new Date();
+console.log(today); // this will gives entire date and time
+
+//get the year
+let date= new Date();
+console.log("Year:",date.getFullYear()); 
+
+//get the month
+console.log("Month:", date.getMonth()); // 0-11 (jan=0)
+
+//get the Day of month
+console.log("Day of Month:",date.getDate()); //1-31
+
+//get the Hours 
+console.log("Hours:",date.getHours()); //0-23
+
+//get Minutes
+console.log("Minutes:",date.getMinutes());
+
+//get Seconds
+console.log("Seconds:",date.getSeconds());
+
+//get Milliseconds
+console.log("Milliseconds:",date.getMilliseconds());
