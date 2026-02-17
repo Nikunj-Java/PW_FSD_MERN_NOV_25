49f8068898c879db1d04ada8eacd34765957372b
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-02-17 17:43:53 +0530

    Error Handling

---
+++ week-12/session-39/04. CustomError.js
@@ -0,0 +1,10 @@
+let age= 15;
+
+try {
+    if(age<18){
+        throw new Error("You Are not eliglible!");
+    }
+    console.log("Access Granted!");
+} catch (error) {
+    console.log("Error Name:",error.message)
+}
---
+++ week-12/session-39/06 ErrorObjects.js
@@ -0,0 +1,8 @@
+try {
+    let x=y;
+    
+} catch (error) {
+    console.log("Error Name:",error.name);
+    console.log("Error Message:",error.message);
+
+}
