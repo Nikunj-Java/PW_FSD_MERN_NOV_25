32da39e0e24f1ae54f89ce6983f3a835a5acb843
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-04-13 19:12:44 +0530

    React

--- week-19/session-61/my-react-app/src/App.tsx
+++ week-19/session-61/my-react-app/src/App.tsx
@@ -3,22 +3,24 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0);
   
-  function handleClick(){
+  function increment(){
     setCount(count + 1);
   }
-   
+
+  function decrement(){
+    setCount(count - 1);
+  }
 
   return (
     <div>
-      <h1>Vite + React Application</h1>
-      <p>My First Vite + React App</p>
-      <p>Here i am Changing DOM</p>
-      <div>
-        This is a simple React application built with Vite. It demonstrates how to set up a React project using Vite as the build tool. You can use this as a starting point for your own React projects and customize it according to your needs.
-      </div>
-      <button onClick={handleClick}>{count}</button> 
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      
+      <button onClick={increment}>Increment </button> 
 
+      <button onClick={decrement}>Decrement </button> 
     </div>
+     
   )
 }
 
