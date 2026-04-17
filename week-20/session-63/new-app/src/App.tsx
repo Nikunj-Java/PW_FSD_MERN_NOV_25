524932b54ec56e01708b341cfea5ef90b7a01aac
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-04-17 17:24:58 +0530

    useState & useEffect

---
+++ week-20/session-63/new-app/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Cart from './cart'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+     <div>
+      <h1>Vite + React</h1>
+      <p>App With useEffect & useState Hook</p>
+      <Cart/>
+     </div>
+  )
+}
+
+export default App
---
+++ week-20/session-63/new-app/src/Cart.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+interface Item{
+    id:number,
+    name:string,
+    price:number
+}
+
+function Cart(){
+    const [cart,setCart] = useState<Item[]>([]);
+    const [total,setTotal] = useState<number>(0);
+
+    const products: Item[]=[
+        {id:1,name:"Shoes",price:2000},
+        {id:2,name:"T-Shirts",price:500},
+        {id:3,name:"Watch",price:3000},
+    ]
+    // add items to the cart
+    const addToCart= (item:Item)=>{
+        setCart([...cart,item]);
+        //setTotal(total+item.price);
+    }
+    useEffect(()=>{
+        const newTotal= cart.reduce((sum,item)=>sum+item.price,0);
+        setTotal(newTotal);
+    },[cart]);
+
+    return(
+        <div>
+            <h2>Products</h2>
+            {products.map((item)=>(
+                <div key={item.id}>
+                    <span>{item.name} - ₹{item.price}</span>
+                    <button onClick={()=>addToCart(item)}>  Add</button>
+                </div>
+            ))}
+
+            <h2>Cart</h2>
+            {cart.map((item)=>(
+                <div key={item.id}>
+                    <span>{item.name} - ₹{item.price}</span>
+                    <button>Remove</button>
+                </div>
+            ))}
+            <h2>Total: ₹{total}</h2>
+        </div>
+    )
+
+}
+export default Cart;
