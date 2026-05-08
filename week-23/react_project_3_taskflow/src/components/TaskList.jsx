30f41219faf5dcecf0fcdf2b07a8628bd96f838a
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-05-08 19:41:37 +0530

    Final Project

--- week-23/react_project_3_taskflow/src/components/TaskList.jsx
+++ week-23/react_project_3_taskflow/src/components/TaskList.jsx
@@ -1,28 +1,71 @@
 import { useState } from "react";
 
 function TaskList({ tasks, dispatch }) {
-  const [editId,setEditId]=useState(null);
-  const [editText,setEditText]=useState("")
-  // start editing
 
-  // save Editing Task
+  const [editId, setEditId] = useState(null);
+  const [editText, setEditText] = useState("");
+
+  // Start Editing
+  const startEdit = (task) => {
+    setEditId(task.id);
+    setEditText(task.text);
+  };
+
+  // Save Edited Task
+  const saveEdit = (id) => {
+    dispatch({
+      type: "EDIT_TASK",
+      payload: {
+        id,
+        text: editText,
+      },
+    });
+
+    setEditId(null);
+    setEditText("");
+  };
+
   return (
     <ul>
       {tasks.map((t) => (
         <li key={t.id}>
-          {t.text}
-          <button>Save</button>
-          <button>Edit</button>
-          <button 
-            onClick={() =>
-              dispatch({
-                type: "DELETE_TASK",
-                payload: t.id,
-              })
-            }
-          >
-            ❌
-          </button>
+
+          {/* Edit Mode */}
+          {editId === t.id ? (
+            <input
+              value={editText}
+              onChange={(e) => setEditText(e.target.value)}
+            />
+          ) : (
+            <span>{t.text}</span>
+          )}
+
+          <div className="task-buttons">
+
+            {/* Edit / Save */}
+            {editId === t.id ? (
+              <button onClick={() => saveEdit(t.id)}>
+                💾 Save
+              </button>
+            ) : (
+              <button onClick={() => startEdit(t)}>
+                ✏ Edit
+              </button>
+            )}
+
+            {/* Delete */}
+            <button
+              onClick={() =>
+                dispatch({
+                  type: "DELETE_TASK",
+                  payload: t.id,
+                })
+              }
+            >
+              ❌ Delete
+            </button>
+
+          </div>
         </li>
       ))}
     </ul>
