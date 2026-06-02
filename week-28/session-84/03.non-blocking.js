875ee63c2c018f32c4dfbaef93267aaf69c79eee
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-06-02 17:23:47 +0530

    Node js

---
+++ week-28/session-84/03.non-blocking.js
@@ -0,0 +1,8 @@
+console.log("Non-Blocking Example");
+
+console.log("Start");
+setTimeout(()=>{
+    console.log("Timer Finished!");
+},5000); //3000 milliseconds= 3seconds
+
+console.log("End")
---
+++ week-28/session-84/04.non-blocking.js
@@ -0,0 +1,16 @@
+console.log("start");
+
+setTimeout(()=>{
+    console.log("i will appear after 5 seconds")
+},5000);
+
+setTimeout(()=>{
+    console.log("i will appear after 3 seconds")
+},3000);
+
+setTimeout(()=>{
+    console.log("i will appear after 1 seconds")
+},1000);
+
+console.log("end");
+// use cases: 1. Pizaa order, Burger Order, Online Food Order
