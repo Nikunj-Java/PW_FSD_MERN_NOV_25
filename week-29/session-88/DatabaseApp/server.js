dfd913352330968e2196d9058073b9bc5732d05b
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-06-12 17:51:06 +0530

    MYSQL

--- week-29/session-88/DatabaseApp/server.js
+++ week-29/session-88/DatabaseApp/server.js
@@ -1,1 +1,11 @@
 import connection from './dbconnect.js';
+
+const sql="INSERT INTO users(name,email) VALUES(?,?)"; // ? is used for dynamic data
+
+connection.query(sql,['Sushant','sn@pw.live'],(err,results)=>{
+    if(err){
+        console.error('Error inserting data:', err);
+        return;
+    }
+    console.log('Data inserted successfully:', results);
+});
