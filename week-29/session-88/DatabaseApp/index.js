729c0af35f9199ac439ca719f0ac77b88c14cb2c
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-06-12 18:52:49 +0530

    NOSQL

--- week-29/session-88/DatabaseApp/index.js
+++ week-29/session-88/DatabaseApp/index.js
@@ -1,1 +1,32 @@
 import db from './mongo.js';
+
+const users=db.collection('users');
+
+/*
+// 01 Insert Data
+const result1= await users.insertOne({
+    name:'Danish Akhtar',
+    email:'danish@pw.live'});
+console.log('Data inserted successfully:', result1);
+*/
+// 02. Read Data
+
+const data= await users.find().toArray();
+
+console.log('Data fetched successfully:', data);
+
+// 03. Read Single Record
+const user= await users.findOne({name:'Nikunj Soni'});
+
+console.log('Data fetched successfully:', user);
+
+
+//04 Update Data
+const result= await users.updateOne({
+    name:'Nikunj Soni'
+},{
+    $set:{email:'nikunjsoni.updated@pw.live'}
+});
+console.log('Data updated successfully:', result);
+
+//05 Delete Data
