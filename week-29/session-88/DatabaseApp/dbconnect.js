dc355fb1bfe59a2d23baac480d43b2a976df6652
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-06-12 17:32:10 +0530

    MYSQL

---
+++ week-29/session-88/DatabaseApp/dbconnect.js
@@ -0,0 +1,19 @@
+// refer the notes and then poroceed with the code
+import mysql from 'mysql2';
+
+const connection= mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Nikunj@123',
+    database:'company',
+});
+
+connection.connect((err)=>{
+    if(err){
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database!');
+})
+
+export default connection;
---
+++ week-29/session-88/DatabaseApp/mysql.md
@@ -0,0 +1,21 @@
+# Install MySQL2 
+```
+npm install mysql2
+```
+## Create Database in MYSQL
+```
+CREATE DATABASE company;
+```
+## Use Database
+```
+use company;
+```
+
+## Create Table 
+```
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    email VARCHAR(100)
+);
+```
