c416ae59cc4e69d8d9ed68530824d89e522bc7ed
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-06-19 18:06:32 +0530

    MONGO DB CRUD APPLICATION

--- week-30/session-91/MongoDB/controller/userController.js
+++ week-30/session-91/MongoDB/controller/userController.js
@@ -60,3 +60,33 @@ export const getUserByID= async(req,res)=>{
         });
     }
 }
+//04. Update user by ID
+export const updateUserByID= async(req,res)=>{
+    try {
+        const user=await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,{
+                new:true,
+                runValidators:true
+            }
+        )
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+        res.json({
+            success: true,
+            data: user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+//05 delete user by ID
--- week-30/session-91/MongoDB/routes/userRoutes.js
+++ week-30/session-91/MongoDB/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { createUser, getAllUsers, getUserByID } from '../controller/userController.js';
+import { createUser, getAllUsers, getUserByID, updateUserByID } from '../controller/userController.js';
 
 const route=express.Router();
 
 route.post("/",createUser);
 route.get("/",getAllUsers);
 route.get("/:id",getUserByID);
+route.put("/:id",updateUserByID);
 
 export default route;
