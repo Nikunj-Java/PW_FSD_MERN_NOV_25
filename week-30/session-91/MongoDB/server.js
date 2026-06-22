d2d0dde194189782256305f05a8275dabff6ca4e
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-06-22 18:02:44 +0530

    Pug, CORS

--- week-30/session-91/MongoDB/server.js
+++ week-30/session-91/MongoDB/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
  
 import mongoose from "mongoose";
 import connectDB from "./config/db.js";
@@ -7,11 +8,19 @@ import useRoutes from "./routes/userRoutes.js"
 import searchRoutes from "./routes/searchRoute.js"
 
 const app= express();
+//cross Origin Dependency
+// npm installa cors 
+// import cors from 'cors'
+app.use(cors({ origin: 'http://127.0.0.1:5500' }));
+//app.use(cors());
+//app.use(cors({ origin: '*' }));
 app.use(express.json());
 connectDB();
 
+
 app.use("/users",useRoutes);
 app.use("/search",searchRoutes);
+
 
 app.listen(3000,()=>
     console.log('Server is Running and up! \nvisit: http://localhost:3000')
