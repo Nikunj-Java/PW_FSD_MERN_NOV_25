6c6aea97cd4e93eec3b7530e52c483b866d7b248
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-06-26 17:20:27 +0530

    Express App

---
+++ week-31/session-94/Express-app/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT=process.env.PORT;
+
+const app= express();
+
+app.use(express.json()); //middleware
+
+app.use("/",(req,res)=>{
+    res.send("Welcome to Express Deployment!")
+})
+
+app.listen(PORT,()=>
+    console.log(`Server is Running And Up! \n: visit: http://localhost:${PORT}`)
+);
