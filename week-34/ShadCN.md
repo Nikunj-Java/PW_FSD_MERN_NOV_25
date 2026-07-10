40e08750aae5fc4cbe59ee79d50994b9dcdecb43
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-07-10 19:27:16 +0530

    Oauth,ShadCN

---
+++ week-34/ShadCN.md
@@ -0,0 +1,34 @@
+# ShadCN
+- before the use of ShadCN we were using Bootstrap
+- so we need to reply on Bootstrap Library
+- bit while we use ShadCN it download it locally the component/library you want to use and you have full control on it
+
+## How to Install ShadCN ?
+```
+npx shadcn@latest init
+```
+- You Will Be Ask Few Questions
+1. Which preset would you like to use ?
+    - Choose Anything You Like
+2. you can see Components/ui and lib/utils.js Folder
+```
+app
+│
+├── components
+│     └── ui
+│
+├── lib
+│     └── utils.ts
+│
+```
+3. Add a Button
+```
+npx shadcn@latest add button
+```
+this will create button.jsx
+```
+components
+│
+└── ui
+      button.jsx
+```
--- week-34/google-auth-demo/app/components/LoginButton.jsx
+++ week-34/google-auth-demo/app/components/LoginButton.jsx
@@ -1,9 +1,26 @@
 "use client";
+import {Button} from "@/components/ui/button";
 
 import { signIn } from "next-auth/react";
 
 export default function LoginButton(){
     return(
-        <button onClick={() => signIn("google",{callbackUrl:"/dashboard"})}>Sign In With Google</button>
-    )
+        <div>
+        <Button onClick={() => signIn("google",{callbackUrl:"/dashboard"})}>Sign In With Google</Button>
+        <br/>
+        <h1>ShadCN Buttons</h1>
+        <Button variant="destructive">Destructive</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="ghost">Ghost</Button>
+        <Button variant="link">Link</Button>
+        <h1>Buttons with size</h1>
+        <Button size="sm">Small</Button>
+        <Button size="lg">Large</Button>
+        <Button size="default">Default</Button>
+        <Button size="icon">+</Button>
+
+
+    </div>
+        )
 }
