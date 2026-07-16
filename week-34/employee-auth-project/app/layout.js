afdf64f19322ff721cf3036de6bd2de6ec7b4b17
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-07-16 19:03:23 +0530

    NEXT JS Production Ready Setup

--- week-34/employee-auth-project/app/layout.js
+++ week-34/employee-auth-project/app/layout.js
@@ -1,14 +1,39 @@
+import { Inter, Roboto } from "next/font/google";
+import localFont from "next/font/local";
+
+const myFont = localFont({
+  src: "./fonts/MyFont.ttf",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+})
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "300",
+    "400",
+    "500",
+    "700",
+    "900",
+  ],
+})
 export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
 
         <h2>
           Employee Management System
         </h2>
 
         <hr />
+        <p className={roboto.className}>
+          Welcome
+        </p>
 
         {children}
 
